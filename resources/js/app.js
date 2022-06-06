8bc0a0013d2ebddf42fd65b8fa10e711e2135915
Guillaume Dery <guillaume@cherrypulp.com>
2022-06-06 15:10:28 +0200

    fix: vueJs not imported

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,21 @@
 require('./bootstrap');
 
 import Alpine from 'alpinejs';
+import { createApp } from 'vue'
+import Vue3Marquee from 'vue3-marquee'
+import 'vue3-marquee/dist/style.css'
+import TopBar from './components/TopBar.vue'
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+
+const app = createApp({})
+
+app.component('top-bar', TopBar)
+
+app.use(Vue3Marquee)
+
+app.mount('#app')
