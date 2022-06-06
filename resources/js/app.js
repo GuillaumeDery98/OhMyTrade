43ff7ae0c7a09280668c78e1ea42aa222c733d20
GuillaumeDery98 <dery.guillaume98@outlook.fr>
2022-06-06 14:27:54 +0200

    feat: basic interface

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,13 @@
 require('./bootstrap');
 import { createApp } from 'vue'
+import Vue3Marquee from 'vue3-marquee'
+import 'vue3-marquee/dist/style.css'
+import TopBar from './components/TopBar.vue'
 
 const app = createApp({})
+
+app.component('top-bar', TopBar)
+
+app.use(Vue3Marquee)
 
 app.mount('#app')
