16fd012df7b56fc5569eb355a81011c443235eb9
GuillaumeDery98 <dery.guillaume98@outlook.fr>
2022-06-21 15:17:15 +0200

    feat: charts begin

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import { createApp } from 'vue'
 import Vue3Marquee from 'vue3-marquee'
 import 'vue3-marquee/dist/style.css'
 import TopBar from './components/TopBar.vue'
+import Chart from './components/Chart.vue'
 
 window.Alpine = Alpine;
 
@@ -15,6 +16,7 @@ Alpine.start();
 const app = createApp({})
 
 app.component('top-bar', TopBar)
+app.component('chart', Chart)
 
 app.use(Vue3Marquee)
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,5 +3,5 @@
 Dashboard
 @endsection
 @section('content')
-test
+<chart></chart>
 @endsection
